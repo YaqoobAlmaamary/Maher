35b2b88ef8b8898a163ebc9fe31fddacd98f132f
unknown <i.su7aim@gmail.com>
2020-02-11 02:06:58 +0300

    add skill input

---
+++ src/components/SkillsInput.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import { View, Text, TouchableOpacity, TextInput, StyleSheet, Platform } from 'react-native'
+import { Item as FormItem, Label, Input } from 'native-base'
+import TextButton from './TextButton'
+
+export default class SkillsInput extends Component {
+  state= {
+    skill: '',
+  }
+
+  sendAndClear = () => {
+    this.props.handleAddSkill(this.state.skill)
+    this.setState({
+      skill: ''
+    })
+  }
+
+  render () {
+    const { disableAdd, skill } = this.state
+
+    return (
+      <View style={styles.container}>
+        <FormItem style={styles.formItem}>
+          <Input
+          value={skill}
+          placeholder="enter at least 1 char to add"
+          style={styles.textInput}
+          autoFocus={this.props.autoFocus == true ? true : false }
+          onChangeText={(skill) => this.setState({skill})} />
+        </FormItem>
+        <View style={styles.buttonContainer}>
+          {skill.trim() == '' ?
+            <TextButton style={styles.textButtonContainer} disabled={true}>Add</TextButton>
+          : <TextButton style={styles.textButtonContainer}
+              onPress={this.sendAndClear}>Add</TextButton>}
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  contrainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  textButtonContainer: {
+    margin: 30,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    alignSelf: 'flex-end'
+  },
+  formItem: {
+    backgroundColor: '#2f2f2f',
+    borderColor: '#2f2f2f',
+    paddingLeft: 5,
+    paddingRight: 50,
+    margin: 15,
+    marginBottom: 0,
+    borderRadius: 4,
+    height: 60,
+
+  },
+  textInput: {
+    flex:1,
+    height: 45,
+    marginLeft: 8,
+  },
+})
