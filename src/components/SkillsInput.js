593e21e293f01832cd35ef6528ae6432144b1b4f
unknown <i.su7aim@gmail.com>
2020-02-11 14:31:34 +0300

    style: increase formItem padding right

--- src/components/SkillsInput.js
+++ src/components/SkillsInput.js
@@ -3,42 +3,22 @@ import { View, Text, TouchableOpacity, TextInput, StyleSheet, Platform } from 'r
 import { Item as FormItem, Label, Input } from 'native-base'
 import TextButton from './TextButton'
 
-<<<<<<< HEAD
+
 export default class SkillsInput extends Component {
   state= {
     skill: '',
   }
 
   sendAndClear = () => {
-=======
-export default class SlillsInput extends Component {
-  state= {
-    disableAdd: false,
-    skill: '',
-  }
-
-  clear = () => {
->>>>>>> d126bd447ecbf6b07d29b3e0f63a2ccf36e6befb
     this.props.handleAddSkill(this.state.skill)
     this.setState({
       skill: ''
     })
   }
-<<<<<<< HEAD
+
 
   render () {
-    const { disableAdd, skill } = this.state
-=======
-  activate = () => {
-    this.setState({
-      disableAdd: false
-    })
-  }
-
-  render () {
-    const { disableAdd, skill } = this.state
-    console.log(skill)
->>>>>>> d126bd447ecbf6b07d29b3e0f63a2ccf36e6befb
+    const { skill } = this.state
 
     return (
       <View style={styles.container}>
@@ -54,11 +34,7 @@ export default class SlillsInput extends Component {
           {skill.trim() == '' ?
             <TextButton style={styles.textButtonContainer} disabled={true}>Add</TextButton>
           : <TextButton style={styles.textButtonContainer}
-<<<<<<< HEAD
               onPress={this.sendAndClear}>Add</TextButton>}
-=======
-              onPress={this.clear}>Add</TextButton>}
->>>>>>> d126bd447ecbf6b07d29b3e0f63a2ccf36e6befb
         </View>
       </View>
     )
@@ -81,19 +57,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#2f2f2f',
     borderColor: '#2f2f2f',
     paddingLeft: 5,
-    paddingRight: 50,
+    paddingRight: 60,
     margin: 15,
     marginBottom: 0,
     borderRadius: 4,
     height: 60,
 
   },
-<<<<<<< HEAD
-=======
-  label: {
-    paddingLeft: 10,
-  },
->>>>>>> d126bd447ecbf6b07d29b3e0f63a2ccf36e6befb
   textInput: {
     flex:1,
     height: 45,
