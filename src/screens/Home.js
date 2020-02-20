a57bfe8e1389dc9e94068b79a046e703df1018a2
unknown <i.su7aim@gmail.com>
2020-02-20 03:11:32 +0300

    test getCurrentuser

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, TouchableOpacity } from 'react-native'
 import { Text } from 'native-base'
+import { withFirebaseHOC } from '../../config/Firebase'
 
 class Home extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Home!</Text>
+        <Text>{this.props.firebase.getCurrentUser().displayName}</Text>
+        <TouchableOpacity onPress={() => this.props.firebase.signOut()}>
+          <Text>signOut</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
 
-export default Home
+export default withFirebaseHOC(Home)
