24e7db001442134105cbe61f103d427d70ab661b
unknown <i.su7aim@gmail.com>
2020-02-29 00:51:07 +0300

    remove getUserDataByUid

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,28 +4,10 @@ import { Text } from 'native-base'
 import { withFirebaseHOC } from '../../config/Firebase'
 
 class Home extends Component {
-  state = {
-    email: '',
-  }
-  componentDidMount() {
-
-    this.props.firebase.getUserDataByUid('j3QNiF8gYeQPVtp4Zofylpkvo0r2')
-      .then((querysnapshot) => {
-        this.setState({
-          email: querysnapshot.data().email
-        })
-      })
-
-  }
-
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>HomeEE!</Text>
-        <Text>{this.state.email}</Text>
-        <TouchableOpacity onPress={() => this.props.firebase.signOut()}>
-          <Text>signOut</Text>
-        </TouchableOpacity>
+        <Text>Home!</Text>
       </View>
     )
   }
