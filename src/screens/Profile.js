df56b16c3d030814b3a9246c003162dba0adfb1d
unknown <i.su7aim@gmail.com>
2020-02-28 23:52:58 +0300

    add drawer, logout, skill tags

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
-import { View, Image, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { Component, useState } from 'react'
+import { View, Image, TouchableOpacity, StyleSheet, FlatList } from 'react-native'
 import { Text, Header, Button } from 'native-base'
+import { MaterialIcons, FontAwesome } from '@expo/vector-icons'
 import { createStackNavigator } from '@react-navigation/stack'
-import { withFirebaseHOC } from '../../config/Firebase'
+import Firebase from '../../config/Firebase'
 import EditProfile from './EditProfile'
+import SkillTag from '../components/SkillTag'
+import AwesomeAlert from 'react-native-awesome-alerts'
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem } from '@react-navigation/drawer'
 
 const Stack = createStackNavigator()
+const Drawer = createDrawerNavigator()
 
 class ProfileInfo extends Component {
   state = {
@@ -13,23 +22,34 @@ class ProfileInfo extends Component {
       username: '',
       firstName: '',
       lastName: '',
-    }
+    },
+    showAlert: false
+
   }
   componentDidMount() {
-    const { firebase } = this.props.route.params
-    firebase.getUserDataByUid(firebase.getCurrentUser().uid)
-      .then((user) => {
+    const firebase = Firebase
+    const uid = firebase.getCurrentUser().uid
+
+    // listen to user date changes
+    firebase.userDataByUid(uid).onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => { // whenever user date is changing
         this.setState({
-          user: user.data()
+          user: change.doc.data()
         })
       })
+    })
   }
   render() {
-    console.log(this.props);
     const { user } = this.state
+    const totalHackathons = 0 //todo: calculate total Hackathons that user has participated in
     this.props.navigation.setOptions({
       headerTitleAlign: 'center',
-      title: user.username
+      title: user.username,
+      headerLeft: () => (
+        <TouchableOpacity style={{marginLeft: 15}} onPress={() => this.props.navigation.openDrawer()}>
+          <Text><MaterialIcons name={'menu'} size={30} /></Text>
+        </TouchableOpacity>
+      )
     })
     return (
       <View style={styles.container}>
@@ -44,19 +64,105 @@ class ProfileInfo extends Component {
               <Text style={styles.editButtonText}>Edit my profile</Text>
             </TouchableOpacity>
           </View>
-          <Text>Profile!</Text>
+          {user.skills &&
+            <Text style={{alignSelf: 'flex-start', marginLeft: 15}}>Skills: {user.skills.length}</Text>}
+          {user.skills &&
+            <FlatList
+              style={{flexDirection: 'row', margin: 5, marginLeft: 20}}
+              horizontal={ true}
+              data={user.skills}
+              renderItem={({item}) => <SkillTag skill={item} /> }
+              keyExtractor={(item) => item}
+            />}
+          <Text style={{alignSelf: 'flex-start', margin: 20, marginLeft: 15}}>Participated in
+            <Text style={{textTransform: 'uppercase', fontSize: 14, letterSpacing: 1.25}}> {totalHackathons} Hackathons</Text>
+          </Text>
         </View>
       </View>
     )
   }
 }
 
-function Profile({firebase}) {
+// const ProfileInfoWithFirebase = withFirebaseHOC(ProfileInfo)
+
+function ProfileStack() {
   return (
     <Stack.Navigator initialRouteName="Profile">
-      <Stack.Screen name="Profile" initialParams={{firebase}} component={ProfileInfo} />
+      <Stack.Screen name="Profile" component={ProfileInfo} />
       <Stack.Screen name="Edit profile" component={EditProfile} />
     </Stack.Navigator>
+  )
+}
+
+class ProfileDrawer extends Component {
+  state={
+    showAlert:false
+  }
+  hideAlert = () => {
+    this.setState({
+      showAlert: false
+    })
+  }
+  showAlert = () => {
+    this.setState({
+      showAlert: true
+    })
+  }
+  render () {
+    return (
+      <View style={{flex:1}}>
+        <Drawer.Navigator initialRouteName="Profile" drawerContent={props => <CustomDrawerContent showAlert={this.state.showAlert} showAlertFunc={this.showAlert} {...props} />}>
+          <Drawer.Screen name="Profile" component={ProfileStack} />
+        </Drawer.Navigator>
+        <LogOutAlert showAlert={this.state.showAlert} hideAlert={this.hideAlert} />
+      </View>
+    )
+  }
+}
+
+function CustomDrawerContent(props){
+
+  return (
+    <DrawerContentScrollView {...props}>
+      <View style={{alignItems: 'center', paddingTop: 20}}>
+        <TouchableOpacity onPress={() => props.showAlertFunc()}>
+          <Text style={{color: "#BB86FC", fontWeight: 'bold', letterSpacing: 1.25}}>Log out <FontAwesome name='sign-out' size={18} /></Text>
+        </TouchableOpacity>
+      </View>
+    </DrawerContentScrollView>
+  )
+}
+
+function LogOutAlert(props) {
+  const { showAlert, hideAlert } = props
+  const [submiting, setSubmiting] = useState(false) // this is called Hook
+
+  return (
+    <AwesomeAlert
+        show={showAlert}
+        showProgress={submiting ? true : false}
+        title="Do you want to logout from Maher?"
+        closeOnTouchOutside={false}
+        closeOnHardwareBackPress={false}
+        showCancelButton={true}
+        showConfirmButton={true}
+        cancelText="Cancel"
+        confirmText="Log out"
+        confirmButtonColor="#BB86FC"
+        cancelButtonColor="#383838"
+        onCancelPressed={() => {
+          hideAlert()
+        }}
+        onConfirmPressed={() => {
+          setSubmiting(true)
+          Firebase.signOut()
+        }}
+        titleStyle={{color: 'rgba(256,256,256,0.87)', fontSize: 18}}
+        contentContainerStyle={{backgroundColor: '#2e2e2e'}}
+        cancelButtonTextStyle={{fontSize: 16}}
+        confirmButtonTextStyle={{fontSize: 16}}
+        progressColor="#BB86FC"
+    />
   )
 }
 
@@ -68,7 +174,8 @@ const styles = StyleSheet.create({
   infoContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#1e1e1e'
+    backgroundColor: '#1e1e1e',
+    elevation: 1
   },
   nameConatiner: {
     alignItems: 'center',
@@ -98,4 +205,4 @@ const styles = StyleSheet.create({
   }
 })
 
-export default withFirebaseHOC(Profile)
+export default ProfileDrawer
