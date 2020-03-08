cb4b80de41f8985badb1940e771e32a6aba0a965
unknown <i.su7aim@gmail.com>
2020-03-09 01:35:42 +0300

    check connection once

--- utils/helper.js
+++ utils/helper.js
@@ -2,34 +2,12 @@ import { Platform } from 'react-native'
 import NetInfo from "@react-native-community/netinfo"
 
 export const checkConnectivity = (connected, notConnected) => {
-  // For Android devices
-  if (Platform.OS === "android") {
-    NetInfo.isConnected.fetch().then(isConnected => {
-      if (isConnected) {
-        connected()
+  NetInfo.isConnected.fetch().then(isConnected => {
+    if (isConnected) {
+      connected()
 
-      } else {
-        notConnected()
-      }
-    })
-  } else {
-    // For iOS devices
-    NetInfo.isConnected.addEventListener(
-      "connectionChange",
-      this.handleFirstConnectivityChange
-    )
-  }
-}
-
-const handleFirstConnectivityChange = isConnected => {
-  NetInfo.isConnected.removeEventListener(
-    "connectionChange",
-    this.handleFirstConnectivityChange
-  );
-
-  if (isConnected === false) {
-    notConnected()
-  } else {
-    connected()
-  }
+    } else {
+      notConnected()
+    }
+  })
 }
