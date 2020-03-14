86331a6c9cf23d12fe22f1807c72a33f5cadd131
unknown <i.su7aim@gmail.com>
2020-03-15 00:01:28 +0300

    add getDuration() to be used in hackathon card

--- utils/helper.js
+++ utils/helper.js
@@ -1,5 +1,6 @@
 import { Platform } from 'react-native'
 import NetInfo from "@react-native-community/netinfo"
+import moment from 'moment'
 
 export const checkConnectivity = (connected, notConnected) => {
   NetInfo.isConnected.fetch().then(isConnected => {
@@ -10,4 +11,27 @@ export const checkConnectivity = (connected, notConnected) => {
       notConnected()
     }
   })
+}
+
+//given time in seconds
+export const getDuration = (startInSeconds, endInSeconds) => {
+  const start = startInSeconds*1000
+  const end = endInSeconds*1000
+  if(moment(start).isValid() && moment(end).isValid()){
+    if(moment(start).format("YYYY") !== moment(end).format("YYYY")){
+      return moment(start).format("ll")+"-"+moment(end).format("ll")
+    }
+    else if(moment(start).format("MM") !== moment(end).format("MM")){
+      return moment(start).format("ll").split(",").join("-"+moment(end).format("ll").slice(0,7))
+    }
+    else if(moment(start).format("DD") !== moment(end).format("DD")){
+      return moment(start).format("ll").split(",").join("-"+moment(end).format("D"))
+    }
+    else {
+      return moment(start).format("ll")
+    }
+  }
+  else {
+    return "Invalid Dates"
+  }
 }
