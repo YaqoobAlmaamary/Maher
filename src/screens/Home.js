9f32ca25966cbf9a3259979c3425f89f1bc3c5cf
Yaqoob Almaamary <eyaqoobal@gmail.com>
2020-03-23 16:19:44 +0300

    updated the home screen
    Added a naviagtion as well as sorting the hackathon events

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,6 +5,9 @@ import { withFirebaseHOC } from '../../config/Firebase'
 import { FlatList } from 'react-native'
 import HackathonCard from '../components/HackathonCard'
 import HackathonPage from '../screens/HackathonPage'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
 
 // This is the home page that the user will see
 // in this script we must display all available hackathons 
@@ -26,6 +29,9 @@ class Home extends Component {
   componentDidMount() {
     this.getHackathons();
   }
+  componentWillUnmount() {
+
+  }
 
   // this function will fill the list of hackathons
   // from the firestore
@@ -37,15 +43,20 @@ class Home extends Component {
     // reference to the hackathons in firestore
     var hackathons = firebase.allHackathons();
     
-    // get hackathon collection from firestore and use the
-    // callback function
-    hackathons.get().then( querySnapshot => {
+    // get hackathon collection where thier status is open
+    // from firestore and use the callback function
+    hackathons.where( "status", "==", "open" ).get().then( querySnapshot => {
       querySnapshot.forEach( ( doc ) => {
+        
         // add hackathons to buffer
         buffer.push( doc.data() );
       } )
     } ).then( () => {
-      // after finishing adding set hackathons as 
+      // sort buffer array so that the
+      // closest hackathon to start will be first
+      buffer.sort( ( a, b ) => { return a.startDateTime - b.startDateTime } );
+
+      // after finishing adding, set hackathons as 
       // buffer
       this.setState( { hackathons: buffer, } );
     } );
@@ -53,22 +64,36 @@ class Home extends Component {
 
   render() {
     // will return a list of all hackathons 
-    // sorted from newest to oldest 
+    // sorted from closest to furthest 
     return (
-      <View style={ {marginTop: 40, } }> 
-      <FlatList
-        data={ this.state.hackathons }
-        renderItem={ 
-          ( {item} ) => 
-          <HackathonCard
-          hackathon={item} 
-          goToHackathon={() => this.props.navigation.navigate("Hackathon Page", {hackathonId: item.hackathonId, name: item.name})}
-          />
-        }
+      <View style={ { flex: 1, marginTop: 40, } }> 
+        <FlatList
+          data={ this.state.hackathons }
+          renderItem={ 
+            ( {item} ) => 
+            <HackathonCard
+            hackathon={item} 
+            goToHackathon={() => this.props.navigation.navigate("Hackathon Page", {hackathonId: item.hackathonId, name: item.name})}
+            />
+          }
         /></View>   
     )
   }
 }
+const HomeWithFirebase = withFirebaseHOC( Home ) // to have firebase as prop
+
+function HomeStack(props) {
+  props.navigation.setOptions({
+    tabBarVisible: true,
+  })
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={ HomeWithFirebase } />
+      <Stack.Screen name="Hackathon Page" component={ HackathonPage } />
+    </Stack.Navigator>
+  );
+}
+
 
 // to have firebase as prop
-export default withFirebaseHOC( Home );
+export default HomeStack;
