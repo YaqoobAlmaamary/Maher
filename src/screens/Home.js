2cdb4015fb4b8d4e2301bc01e7466a193ab22c5d
unknown <i.su7aim@gmail.com>
2020-03-23 17:38:26 +0300

    add keyExtractor, fix sort by using seconds attribute

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,13 +10,13 @@ import { createStackNavigator } from '@react-navigation/stack'
 const Stack = createStackNavigator()
 
 // This is the home page that the user will see
-// in this script we must display all available hackathons 
+// in this script we must display all available hackathons
 // sorted from newest to oldest as well as a button to add new hackathons
 class Home extends Component {
 
   constructor( props ) {
     super( props );
-    
+
     this.state = {
       // This will hold the list of hackathons to be displayed
       hackathons : [],
@@ -39,44 +39,45 @@ class Home extends Component {
     var buffer = [];
     // the firebase passed through context
     const { firebase } = this.props;
-    
+
     // reference to the hackathons in firestore
     var hackathons = firebase.allHackathons();
-    
+
     // get hackathon collection where thier status is open
     // from firestore and use the callback function
     hackathons.where( "status", "==", "open" ).get().then( querySnapshot => {
       querySnapshot.forEach( ( doc ) => {
-        
+
         // add hackathons to buffer
         buffer.push( doc.data() );
       } )
     } ).then( () => {
       // sort buffer array so that the
       // closest hackathon to start will be first
-      buffer.sort( ( a, b ) => { return a.startDateTime - b.startDateTime } );
+      buffer.sort( ( a, b ) => { return a.startDateTime.seconds - b.startDateTime.seconds } );
 
-      // after finishing adding, set hackathons as 
+      // after finishing adding, set hackathons as
       // buffer
       this.setState( { hackathons: buffer, } );
     } );
   }
 
   render() {
-    // will return a list of all hackathons 
-    // sorted from closest to furthest 
+    // will return a list of all hackathons
+    // sorted from closest to furthest
     return (
-      <View style={ { flex: 1, marginTop: 40, } }> 
+      <View style={ { flex: 1, marginTop: 40, } }>
         <FlatList
           data={ this.state.hackathons }
-          renderItem={ 
-            ( {item} ) => 
-            <HackathonCard
-            hackathon={item} 
-            goToHackathon={() => this.props.navigation.navigate("Hackathon Page", {hackathonId: item.hackathonId, name: item.name})}
-            />
+          renderItem={
+            ( {item} ) => (
+              <HackathonCard
+              hackathon={item}
+              goToHackathon={() => this.props.navigation.navigate("Hackathon Page", {hackathonId: item.hackathonId, name: item.name})}
+              />)
           }
-        /></View>   
+          keyExtractor={(item) => item.hackathonId}
+        /></View>
     )
   }
 }
