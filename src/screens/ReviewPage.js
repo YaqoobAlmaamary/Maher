0884b98bd880ce014da1326786e8d57d23b562e2
MohammedAlaqil <iimaykee@gmail.com>
2020-04-19 20:10:45 +0300

    Review page

--- src/screens/ReviewPage.js
+++ src/screens/ReviewPage.js
@@ -14,70 +14,114 @@ class ReviewPage extends Component {
   state = {
     criteria : [],
     value : "",
-    valueError: ""
+    valueError: "",
+    submiting : false,
+    criteriaObj : []
+
+    }
+
+
+
+
+  reviewTeam = async () => {
+
+  
+
+    this.setState({submiting: true})
+
+    const { teamId , hackathonId } = this.props.route.params
+    const hackathonCollection = await this.props.firebase.getHackathonDoc(hackathonId).get()
+    const { uid } = this.props.firebase.getCurrentUser()
+
+    let criteria = []
+
+    this.state.criteria.map((criteriaObj) => {
+
+      const criteriaData = {
+
+        criteriaId: criteriaObj.criteriaId,
+        value: criteriaObj.value
+
+    }
+
+      criteria = criteria.concat(criteriaData)
+
+      })
+      this.setState({
+        criteriaObj: criteria
+        })
+
+  const updatedData = {
+
+        reviews : {
+           uid : {
+            judgeId : uid,
+            review :  this.state.criteriaObj
+          }
+          }
+}
+
+      await this.props.firebase.getTeamDoc(hackathonId, teamId).update(updatedData)
+      this.props.navigation.goBack()
   }
 
+  async componentDidMount(){
 
-   async componentDidMount(){
+    const { hackathonId } = this.props.route.params
+    const hackathonCollection = await this.props.firebase.getHackathonDoc(hackathonId).get()
+    let criteria = []
 
-  const { teamId , hackathonId } = this.props.route.params
-  const hackathonCollection = await this.props.firebase.getHackathonDoc(hackathonId).get()
-  let criteria = []
+    hackathonCollection.data().criteria.map( (criteriaObj) => {
 
-  hackathonCollection.data().criteria.map( (criteriaObj) => {
-    const criteriaData = criteriaObj
-    criteriaData['criteriaId'] = criteriaObj.criteriaId
-    criteriaData['name'] = criteriaObj.name
-    criteriaData['description'] = criteriaObj.description
-    criteriaData['weight'] = criteriaObj.weight
-    criteria = criteria.concat(criteriaData)
-  })
+      criteria = criteria.concat(criteriaObj)
 
-  this.setState({
-    criteria: criteria
     })
 
-
+    this.setState({
+      criteria: criteria
+      })
 
 }
 
 render (){
-  const { criteria } = this.state
+  const { criteria , submiting, valueError} = this.state
 
   return (
-
 
     <ScrollView>
       <Form>
 
-
-
-
       <FlatList
           data={ this.state.criteria }
           renderItem={ ({item}) =>
-          // for each element in criteria array
-          // render it an input place
 
           <View>
-              <H3>{item.name}</H3>
+              <H3 style={styles.label}>{item.name}</H3>
               <TextInputWithMsg
+                  key= {item.criteriaId}
                   label={"Out of " + item.weight }
+                  error={valueError}
                   value={ this.state.value }
                   onChangeText={ (value) => {
-
+                    item.value = value
                       this.setState( {
-                         value : item.value
+                        value : item,
+                        valueError: ""
                        })
-
-
-                  }}
-
+                  }
+                }
               />
+
               </View>
             }
-
             />
+
+            {submiting ?
+              <ActivityIndicator style={{margin: 25}} size="large" color='#BB86FC' />
+            : <Button style={styles.btn} onPress={this.reviewTeam}>
+                <Text style={styles.btnText}>Submit</Text>
+              </Button>
+            }
 
 
       </Form>
@@ -88,6 +132,30 @@ render (){
   )
 }
 }
+
+const styles = StyleSheet.create({
+
+  label: {
+    marginTop: 15,
+    marginLeft: 15,
+    marginBottom: 5
+  },
+  btn: {
+    alignSelf: 'center',
+    justifyContent:'center',
+    borderRadius: 5,
+    margin: 15,
+    paddingRight: 15,
+    paddingLeft: 15
+  },
+  btnText: {
+    color: '#1e1e1e',
+    fontWeight: 'bold',
+    fontSize: 14,
+    letterSpacing: 1.25,
+    textTransform: 'uppercase'
+  },
+})
 
 
 export default withFirebaseHOC( ReviewPage );
