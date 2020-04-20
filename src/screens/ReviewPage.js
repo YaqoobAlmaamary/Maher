27a8911eae22e7b3e767b8655803aefd133863c5
unknown <i.su7aim@gmail.com>
2020-04-20 03:28:12 +0300

    fix ReviewPage

--- src/screens/ReviewPage.js
+++ src/screens/ReviewPage.js
@@ -1,6 +1,7 @@
 import React, {Component } from 'react'
 import { View, StyleSheet , ScrollView , ActivityIndicator , FlatList} from 'react-native'
 import { Text, Button } from 'native-base'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { MaterialCommunityIcons, FontAwesome, Ionicons, Entypo } from '@expo/vector-icons'
 import { TextInputWithMsg, TextArea } from '../components/Inputs';
 import { Form , H3 } from 'native-base';
@@ -15,122 +16,144 @@ class ReviewPage extends Component {
     criteria : [],
     value : "",
     valueError: "",
-    submiting : false,
-    criteriaObj : []
-
-    }
-
-
-
-
-  reviewTeam = async () => {
-
-  
-
-    this.setState({submiting: true})
-
-    const { teamId , hackathonId } = this.props.route.params
-    const hackathonCollection = await this.props.firebase.getHackathonDoc(hackathonId).get()
-    const { uid } = this.props.firebase.getCurrentUser()
-
-    let criteria = []
-
-    this.state.criteria.map((criteriaObj) => {
-
-      const criteriaData = {
-
-        criteriaId: criteriaObj.criteriaId,
-        value: criteriaObj.value
-
-    }
-
-      criteria = criteria.concat(criteriaData)
-
-      })
-      this.setState({
-        criteriaObj: criteria
-        })
-
-  const updatedData = {
-
-        reviews : {
-           uid : {
-            judgeId : uid,
-            review :  this.state.criteriaObj
-          }
-          }
-}
-
-      await this.props.firebase.getTeamDoc(hackathonId, teamId).update(updatedData)
-      this.props.navigation.goBack()
+    toSubmit: null,
+    isSubmiting: false,
+    isReady: false
   }
 
-  async componentDidMount(){
+  handleReviewChange = (c, input) => {
 
-    const { hackathonId } = this.props.route.params
-    const hackathonCollection = await this.props.firebase.getHackathonDoc(hackathonId).get()
-    let criteria = []
+    let toSubmit = this.state.toSubmit
+    const number = input === "" ? '' : parseInt(input)
+    const prevNumber = toSubmit.review[c.criteriaId].value
 
-    hackathonCollection.data().criteria.map( (criteriaObj) => {
-
-      criteria = criteria.concat(criteriaObj)
-
-    })
+    toSubmit.review[c.criteriaId] = {
+      criteriaId: c.criteriaId,
+      value: (isNaN(number) || number > 100) ? prevNumber : number,
+      weight: c.weight
+    }
 
     this.setState({
-      criteria: criteria
+      toSubmit: toSubmit
+    })
+
+  }
+
+  isThereEmpty = () => {
+    const { toSubmit } = this.state
+    let isEmpty = false
+
+    if(toSubmit == null)
+      isEmpty = true
+    else {
+
+      Object.values(toSubmit.review).map(c => {
+        if(c.value === null || c.value === '')
+          isEmpty = true
       })
+    }
+    return isEmpty
+
+  }
+
+  submitReview = async () => {
+    this.setState({isSubmiting: true})
+    const { uid } = this.props.firebase.getCurrentUser()
+    const { toSubmit } = this.state
+    const { teamId , hackathonId } = this.props.route.params
+    const teamRef = this.props.firebase.getTeamDoc(hackathonId, teamId)
+    const teamDoc = await teamRef.get()
+    const prevReviews = teamDoc.data().reviews
+
+    let updatedData = {}
+
+    if(prevReviews === null)
+      updatedData[uid] = toSubmit
+    else {
+      prevReviews[uid] = toSubmit
+      updatedData = prevReviews
+    }
+
+    await teamRef.update({reviews: updatedData})
+    this.props.navigation.goBack()
+  }
+
+ async componentDidMount(){
+
+  const { teamId , hackathonId, teamName } = this.props.route.params
+  const hackathonCollection = await this.props.firebase.getHackathonDoc(hackathonId).get()
+  const criteria = hackathonCollection.data().criteria
+
+  let toSubmit = {
+    judgeId: this.props.firebase.getCurrentUser().uid,
+    review: {}
+  }
+
+  criteria.map(c => {
+    toSubmit.review[c.criteriaId] = {
+      criteriaId: c.criteriaId,
+      value: null,
+      weight: c.weight
+    }
+  })
+
+  this.setState({
+    criteria: criteria,
+    toSubmit: toSubmit,
+    isReady: true
+  })
+
+  this.props.navigation.setOptions({
+    title: "Review "+teamName,
+    headerTitleAlign: 'center'
+  })
+
 
 }
 
-render (){
-  const { criteria , submiting, valueError} = this.state
+  render (){
+    const { criteria, toSubmit, isSubmiting, isReady } = this.state
+    if(!isReady){
+      return (
+        <ActivityIndicator style={{margin: 25}} size="large" color='#BB86FC' />
+      )
+    }
+    if(criteria.length === 0)
+      return null
+    return (
+      <KeyboardAwareScrollView>
+        {criteria.map( (item) => (
+          <ReviewInput
+            key={item.criteriaId}
+            criteria={item}
+            handleReviewChange={this.handleReviewChange}
+            value={toSubmit == null ? "" : toSubmit.review[item.criteriaId].value} />
+        ))
+        }
+        {isSubmiting ?
+          <ActivityIndicator style={{margin: 25}} size="large" color='#BB86FC' />
+        : <Button style={styles.btn} onPress={this.submitReview} disabled={this.isThereEmpty()}>
+            <Text style={styles.btnText}>Submit</Text>
+          </Button>
+        }
+      </KeyboardAwareScrollView>
+    )
+  }
+}
 
+function ReviewInput({criteria, handleReviewChange, value}){
   return (
-
-    <ScrollView>
       <Form>
-
-      <FlatList
-          data={ this.state.criteria }
-          renderItem={ ({item}) =>
-
-          <View>
-              <H3 style={styles.label}>{item.name}</H3>
-              <TextInputWithMsg
-                  key= {item.criteriaId}
-                  label={"Out of " + item.weight }
-                  error={valueError}
-                  value={ this.state.value }
-                  onChangeText={ (value) => {
-                    item.value = value
-                      this.setState( {
-                        value : item,
-                        valueError: ""
-                       })
-                  }
-                }
-              />
-
-              </View>
-            }
-            />
-
-            {submiting ?
-              <ActivityIndicator style={{margin: 25}} size="large" color='#BB86FC' />
-            : <Button style={styles.btn} onPress={this.reviewTeam}>
-                <Text style={styles.btnText}>Submit</Text>
-              </Button>
-            }
-
-
+        <Text style={styles.label}>{criteria.name}</Text>
+        <TextInputWithMsg
+          keyboardType={"numeric"}
+          label={"Out of 100"}
+          value={ value === null ? "" : value.toString() }
+          onChangeText={(number) => handleReviewChange(criteria, number)}
+          maxLength={3}
+        />
       </Form>
-   </ScrollView>
-
-
-
   )
-}
 }
 
 const styles = StyleSheet.create({
@@ -156,6 +179,5 @@ const styles = StyleSheet.create({
     textTransform: 'uppercase'
   },
 })
-
 
 export default withFirebaseHOC( ReviewPage );
