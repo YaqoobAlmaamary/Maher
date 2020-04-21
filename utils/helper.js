a3a8d02e4ead1e3bf98ce009156c29b7975d008c
unknown <i.su7aim@gmail.com>
2020-04-21 05:52:14 +0300

    add checkDates, checkHackathon, uriToBlob functions

--- utils/helper.js
+++ utils/helper.js
@@ -35,3 +35,140 @@ export const getDuration = (startInSeconds, endInSeconds) => {
     return "Invalid Dates"
   }
 }
+
+export const checkHackathonDates = (startDateTime, endDateTime, reviewStartDateTime, reviewEndDateTime) => {
+  let isError, text
+  if(moment(moment(new Date())).diff(moment(startDateTime.getTime())) >= 0){
+    isError = true
+    text = "Start time should be in the future"
+  }
+  else if(moment(startDateTime.getTime()).diff(moment(endDateTime.getTime())) >= 0){
+    isError = true
+    text = "End time should be after the start time"
+  }
+  else if(moment(endDateTime.getTime()).diff(moment(reviewStartDateTime.getTime())) >= 0){
+    isError = true
+    text = "Review start time should be after the end time"
+  }
+  else if(moment(reviewStartDateTime.getTime()).diff(moment(reviewEndDateTime.getTime())) >= 0){
+    isError = true
+    text = "Review end time should be after the review start time"
+  }
+  else {
+    isError = false
+    text = ""
+  }
+  return {isError: isError, text: text}
+
+}
+
+export const mergeDateTime = (dateTimestamp, timeTimeStamp) => {
+  const date = new Date(dateTimestamp)
+  const time = new Date(timeTimeStamp)
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate(),
+                    time.getHours(), time.getMinutes())
+}
+
+export const checkHackathon = (hackathon) => {
+  const validateDates = checkHackathonDates(hackathon.startDateTime, hackathon.endDateTime, hackathon.reviewStartDateTime, hackathon.reviewEndDateTime)
+  let isError = false
+  let text = ""
+  if(hackathon.name.trim() === ""){
+    isError = true
+    text = "Hackathon name can't be blank"
+  }
+  else if(hackathon.tagline.trim() === ""){
+    isError = true
+    text = "Tagline can't be blank"
+  }
+  else if(hackathon.city.trim() === ""){
+    isError = true
+    text = "City can't be blank"
+  }
+  else if(isNaN(hackathon.maxTeams) || hackathon.maxTeams === 0){
+    isError = true
+    text = hackathon.maxTeams === 0 ? "Max teams can't be zero or blank" : "Max teams should only contains numbers"
+  }
+  else if(isNaN(hackathon.maxInTeam) || hackathon.maxInTeam === 0 || hackathon.maxInTeam < hackathon.minInTeam){
+    isError = true
+    text = hackathon.maxInTeam === 0 ?
+      "Max number of members in a team can't be zero or blank"
+    : hackathon.maxInTeam < hackathon.minInTeam ?
+          "Max number of members is smaller than the minimum"
+        : "Max number of members field should only contains numbers"
+  }
+  else if(isNaN(hackathon.minInTeam) || hackathon.minInTeam === 0){
+    isError = true
+    text = hackathon.minInTeam === 0 ? "Min number of members in a team can't be zero or blank" : "Min number of members field should only contains numbers"
+  }
+  else if(validateDates.isError){
+    isError = true
+    text = validateDates.text
+  }
+  else if(hackathon.criteria.length !== 0){
+    hackathon.criteria.map((c, i) => {
+      if(c.name.trim() === ""){
+        isError = true
+        text = `Criterion number ${i+1} missing the name`
+      }
+      else if(c.weight.trim() === "" || isNaN(c.weight)){
+        isError = true
+        text = isNaN(c.weight) ? `Criterion number ${i+1} weight have invalid value, it should be a number ` : `Criterion number ${i+1} missing the weight`
+      }
+    })
+  }
+  else if(hackathon.currency !== "non-cash" || hackathon.currency === ""){
+    if(hackathon.currency === ""){
+      isError = true
+      text = `Please select currency`
+    }
+    else if(isNaN(hackathon.totalPrizes) || hackathon.totalPrizes.trim() === ""){
+      isError = true
+      text = `Currency selected is ${hackathon.currency} and Estimated Total Prizes is blank or contains chars ( it should only be a number)`
+    }
+  }
+  else if(hackathon.prizes.length !== 0){
+    hackathon.prizes.map( p => {
+      if(p.type === 'cash' && isNaN(p.value)){
+        isError = true
+        text = `Value of prize number ${p.position} has the type of (cash) the value should only be a number`
+      }
+      else if(p.value == 0 && p.type === 'cash'){
+        isError = true
+        text = `Value of prize number ${p.position} has the type of (cash) the value can't be blank or zero`
+      }
+      else if(p.value.trim() === ""){
+        isError = true
+        text = `Value of prize number ${p.position} can't be blank`
+      }
+    })
+  }
+
+  return {isError, text}
+}
+
+export const uriToBlob = (uri) => {
+
+  return new Promise((resolve, reject) => {
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = function() {
+      // return the blob
+      resolve(xhr.response);
+    };
+
+    xhr.onerror = function() {
+      // something went wrong
+      reject(new Error('uriToBlob failed'));
+    };
+
+    // this helps us get a blob
+    xhr.responseType = 'blob';
+
+    xhr.open('GET', uri, true);
+    xhr.send(null);
+
+  });
+
+}
